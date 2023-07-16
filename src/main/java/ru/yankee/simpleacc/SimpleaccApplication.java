bac3161791bb6f65f558c3f57012f6b39d7c5421
Anton Evlampeev <evlampejev@gmail.com>
2023-07-15 23:10:47 -0500

    init

---
+++ src/main/java/ru/yankee/simpleacc/SimpleaccApplication.java
@@ -0,0 +1,13 @@
+package ru.yankee.simpleacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleaccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleaccApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yankee/simpleacc/SimpleaccApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yankee.simpleacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleaccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
